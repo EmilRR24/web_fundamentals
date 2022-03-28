e68da80098c6cba6afc2099e534ee2b4a39ff81d
EmilRR24 <grimlei2005@yahoo.com>
2022-03-27 22:24:56 -1000

    completed button clicker activity

---
+++ html/html_js_css/button_clicker/main.js
@@ -0,0 +1,9 @@
+function logout(element){
+    element.innerText = "Logout";
+}
+function hide(element){
+    element.remove();
+}
+function like(){   
+    window.alert("Ninja was liked");
+}
---
+++ html/html_js_css/connecting_js/main.js
@@ -0,0 +1,20 @@
+// var fullName = "Emil R";
+
+// console.log(fullName);
+
+// for(var i = 0; i < 255; i++){
+//     console.log(i);
+// }
+
+// VANILLA JS METHODS
+
+// alert("Welcome to my Page!!!")
+
+var response = confirm("Are you sure you want to delete this?")
+
+if(response){
+    console.log("Okay, I hope you know what you are doing!")
+}
+else{
+    console.log("Okay, Delete has been cancelled!")
+}
