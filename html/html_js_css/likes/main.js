eb87cb1291123342a93982c8b94b0f677efe2b32
EmilRR24 <grimlei2005@yahoo.com>
2022-03-29 13:12:55 -1000

    completed likes button assignment

---
+++ html/html_js_css/likes/main.js
@@ -0,0 +1,21 @@
+var num = 9
+function onClickHandler(){
+    var likes = document.querySelector(".likes")
+        num++
+        likes.innerText = num + " Like(s)";
+        console.log(num);
+}
+var num2 = 12
+function onClickHandler2(){
+    var likes = document.querySelector(".likes2")
+        num2++
+        likes.innerText = num2 + " Like(s)";
+        console.log(num2);
+}
+var num3 = 9
+function onClickHandler3(){
+    var likes = document.querySelector(".likes3")
+        num3++
+        likes.innerText = num3 + " Like(s)";
+        console.log(num3);
+}
