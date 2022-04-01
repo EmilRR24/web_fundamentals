e812e2cfe1aefbc409bca6b0de03d00a7ad2fa6d
EmilRR24 <grimlei2005@yahoo.com>
2022-03-31 14:43:00 -1000

    created a project using onclick

---
+++ projects/click/main.js
@@ -0,0 +1,41 @@
+function game(element){
+    var red = document.querySelector('.targetRed');
+    red.style.display = 'block';
+    element.style.display = 'none';
+}
+function next(element){
+    var x = document.querySelector('.targetGreen-2');
+    x.style.display = 'block';
+    element.style.display = 'none';
+}
+function next2(element){
+    var x = document.querySelector('.targetBlue-2');
+    x.style.display = 'block';
+    element.style.display = 'none';
+}
+function next3(element){
+    var x = document.querySelector('.targetGreen');
+    x.style.display = 'block';
+    element.style.display = 'none';
+}
+function next4(element){
+    var x = document.querySelector('.targetRed-2');
+    x.style.display = 'block';
+    element.style.display = 'none';
+}
+function next5(element){
+    var x = document.querySelector('.targetBlue');
+    x.style.display = 'block';
+    element.style.display = 'none';
+}
+function next6(element){
+    var x = document.querySelector('#restart');
+    x.style.display = 'block';
+    element.style.display = 'none';
+}
+function restart(element){
+    var x = document.querySelector('#startGame');
+    x.style.display = 'block';
+    element.style.display = 'none';
+}
+
